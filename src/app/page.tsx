77ed68c0bd2865ffbb8d4125e7aa5a81b0df936b
tll471 <brodskyyury@gmail.com>
2026-06-08 09:40:11 +0300

    start

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <img src="./pictures/gaza.jfif" alt="dd" className="flex size-100 m-0 m-auto mt-10"/>
+    </div>
+  );
+}
