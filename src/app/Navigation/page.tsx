563f03caf31accc4c2e5604c44d14a467121ae73
tll471 <brodskyyury@gmail.com>
2026-06-08 10:37:58 +0300

    вв

--- src/app/Navigation/page.tsx
+++ src/app/Navigation/page.tsx
@@ -2,10 +2,10 @@ import Link from "next/link";
 
 export function Navigation() {
   return (
-    <div className="flex flex-row gap-5 m-0 m-auto">
-      <Link href='/about'>Інтернет-магазин</Link>
-      <Link href='/about'>Огляд</Link>
-      <Link href='/about'>Допомога</Link>
+    <div className="flex flex-row justify-center gap-5 bg-blue-100">
+    <Link href="/about">Інтернет-магазин</Link>
+    <Link href="/about">Огляд</Link>
+    <Link href="/about">Допомога</Link>
     </div>
   );
 }
